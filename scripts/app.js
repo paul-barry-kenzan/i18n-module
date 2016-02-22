c17f9dcb77ad83db1073e32e1eead2847cc8b750
Paul Barry <pbarry@kenzan.com>
2016-02-22 16:19:36 -0500

    Implement translation

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function (angular) {
+
+  angular
+    .module('i18nExampleApp', ['km.i18n'])
+    .config(function($translateProvider, tmhDynamicLocaleProvider, LocaleServiceProvider){
+      $translateProvider.useMissingTranslationHandlerLog();
+
+      $translateProvider.useStaticFilesLoader({
+        prefix: './translations/locale-',
+        suffix: '.json'
+      });
+
+      $translateProvider.determinePreferredLanguage();
+      $translateProvider.useLocalStorage();
+
+      tmhDynamicLocaleProvider.localeLocationPattern('../bower_components/angular-i18n/angular-locale_{{locale}}.js');
+
+      // jshint camelcase: false
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      LocaleServiceProvider.setSupportedLocales({
+        ru_RU: 'Русский',
+        es_ES: 'Espanol'
+      });
+    });
+
+})(angular);
